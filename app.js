52546e652220e8cfe025143f57a26637e2b20297
Himanshikapurwar <himanshikapurwar28@gmail.com>
2021-09-16 22:19:20 +0530

    Level-2

--- app.js
+++ app.js
@@ -1,60 +1,70 @@
 //jshint esversion:6
- const express = require("express");
- const bodyParser = require("body-parser");
- const ejs = require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
- const app = express();
+const app = express();
 
- const mongoose = require("mongoose");
- app.use(express.static("public"));
- app.set("view engine", "ejs");
- app.use(bodyParser.urlencoded({
-   extended: true
- }));
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
- mongoose.connect('mongodb://localhost:27017/userDB');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB');
 
 
- const userSchema = {
-   email: String,
-   password: String
- };
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
 
- const User = new mongoose.model("User", userSchema);
+const secret = "Thisismysecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedField: ["password"]});
 
- app.get("/", function(req, res){
-   res.render("home");
- });
+const User = new mongoose.model("User", userSchema);
 
- app.get("/login", function(req, res){
-   res.render("login")
- });
+app.get("/", function(req, res) {
+  res.render("home");
+});
 
- app.get("/register", function(req, res){
-   res.render("register")
- });
+app.get("/login", function(req, res) {
+  res.render("login")
+});
 
-app.post("/register", function(req, res){
-  const newUser =  new User({
+app.get("/register", function(req, res) {
+  res.render("register")
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
     email: req.body.username,
     password: req.body.password
   });
-  newUser.save(function(err){
-    if(err){console.log(err);}
-    else{res.render("secrets")}
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets")
+    }
   });
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if (err){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
     }
-    if(foundUser) {
-      if (foundUser.password === password){
+    if (foundUser) {
+      if (foundUser.password === password) {
         res.render("secrets");
       }
     }
@@ -69,7 +79,6 @@ app.post("/login", function(req, res){
 
 
 
-
- app.listen(4000, function(){
-   console.log("Server statred at port 4000");
- });
+app.listen(4000, function() {
+  console.log("Server statred at port 4000");
+});
